fa599f2375f291afc4070b39f5ded24502cc5d68
ADE YOGA WIYADNYANA <yorisofficial@gmail.com>
2021-11-26 08:07:53 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,1 @@
 var myModal = new bootstrap.Modal(document.getElementById("modal-hero"), {});
-
-document.onreadystatechange = function () {
-myModal.show();
-};
-
-if (document.querySelectorAll('[data-bss-baguettebox]').length > 0) {
-   baguetteBox.run('[data-bss-baguettebox]', { animation: 'slideIn' });
-}
